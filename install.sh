f2d1054263d22181d2290ced49595d90719b4036
Fajar Destra Prayoga <fajardprayoga@protonmail.com>
2025-07-27 02:56:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ pacman -Sy reflector --noconfirm
 reflector --country Indonesia --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 echo "[+] Install base system"
-pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode nano sudo networkmanager grub efibootmgr btrfs-progs git bash
+pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode nano sudo networkmanager grub efibootmgr btrfs-progs git bash tzdata
 
 echo "[+] Generate fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
