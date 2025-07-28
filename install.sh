181422b1972bc565eba3213abdeacb8c0f8080bb
Fajar Destra Prayoga <fajardprayoga@protonmail.com>
2025-07-28 16:23:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,20 +31,20 @@ btrfs subvolume create /mnt/@swap
 umount /mnt
 
 echo "[+] Mounting subvolumes"
-mount -o noatime,compress=zstd,subvol=@ $ROOT_PART /mnt
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ $ROOT_PART /mnt
 mkdir -p /mnt/{home,var,tmp,opt,srv,swap}
-mount -o noatime,compress=zstd,subvol=@home $ROOT_PART /mnt/home
-mount -o noatime,compress=zstd,subvol=@var $ROOT_PART /mnt/var
-mount -o noatime,compress=zstd,subvol=@tmp $ROOT_PART /mnt/tmp
-mount -o noatime,compress=zstd,subvol=@opt $ROOT_PART /mnt/opt
-mount -o noatime,compress=zstd,subvol=@srv $ROOT_PART /mnt/srv
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home $ROOT_PART /mnt/home
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var $ROOT_PART /mnt/var
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@tmp $ROOT_PART /mnt/tmp
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@opt $ROOT_PART /mnt/opt
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@srv $ROOT_PART /mnt/srv
 mount -o noatime,nodatacow,compress=no,subvol=@swap $ROOT_PART /mnt/swap
 mkdir -p /mnt/var/{log,cache,tmp}
-mount -o noatime,compress=zstd,subvol=@log $ROOT_PART /mnt/var/log
-mount -o noatime,compress=zstd,subvol=@cache $ROOT_PART /mnt/var/cache
-mount -o noatime,compress=zstd,subvol=@var_tmp $ROOT_PART /mnt/var/tmp
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@log $ROOT_PART /mnt/var/log
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@cache $ROOT_PART /mnt/var/cache
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var_tmp $ROOT_PART /mnt/var/tmp
 mkdir -p /mnt/var/cache/pacman/pkg
-mount -o noatime,compress=zstd,subvol=@pkg $ROOT_PART /mnt/var/cache/pacman/pkg
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@pkg $ROOT_PART /mnt/var/cache/pacman/pkg
 
 echo "[+] Mount EFI partition"
 mkdir -p /mnt/boot/efi
