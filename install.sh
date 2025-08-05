784c9195d3ec8ae20412952da39390fd5b2ebf9f
Fajar Destra Prayoga <fajardprayoga@protonmail.com>
2025-08-05 00:05:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ DISK="/dev/nvme0n1"
 EFI_PART="${DISK}p1"
 SWAP_PART="${DISK}p2"
 ROOT_PART="${DISK}p3"
-HOSTNAME="fajardp-archlinux"
+HOSTNAME="fajardp-archlinux-pc"
 USERNAME="fajar"
 ROOT_PASS="r!N4@O50689"
-USER_PASS="050689"
+USER_PASS="mamah@R1na25"
 
 echo "[+] Format BTRFS on $ROOT_PART"
 mkfs.btrfs -f -L archlinux $ROOT_PART
