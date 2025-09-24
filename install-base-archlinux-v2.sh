a86b87b31022a9be2732ec387ec313aaab669d13
Fajar Destra Prayoga <fajardprayoga@protonmail.com>
2025-09-24 11:25:16 +0700

    Update install-base-archlinux-v2.sh

--- install-base-archlinux-v2.sh
+++ install-base-archlinux-v2.sh
@@ -7,8 +7,8 @@ SWAP_PART="/dev/nvme0n1p2"    # Swap Partisi
 ROOT_PART="/dev/nvme0n1p3"    # Root (BTRFS)
 HOSTNAME="fajardp-archlinux-pc"
 USERNAME="fajar"
-ROOT_PASS="r!N4@O50689#25"
-USER_PASS="050689"
+ROOT_PASS="pass"
+USER_PASS="pass"
 
 # Opsi format partisi (ubah ke true/false sesuai kebutuhan)
 FORMAT_EFI=true         # true jika ingin format ulang ESP
